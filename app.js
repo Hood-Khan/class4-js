1ed8d0722fb1a224ddb3d198b35e96d99c9ceb3e
HoodKhan <muhammadhoodkhan9366@gmail.com>
2025-02-18 18:08:27 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let marks = Number(prompt('Please enter marks: '))
+
+if(marks>=90 && marks<=100){
+    console.log("A1 Grade")
+}
+else if(marks>=80 && marks<90){
+    console.log('A Grade')
+}
+else if(marks>=70 && marks<80){
+    console.log('B Grade')
+}
+else if(marks>=60 && marks<70){
+    console.log('C Grade')
+}
+else if(marks>=50 && marks<60){
+    console.log('Pass')
+}
+else if(marks>=30 && marks<50){
+    console.log('Pass')
+}
+else if(marks>=0 && marks<30){
+    console.log("fail")
+}
+else{
+    console.log('invalid marks')
+}
