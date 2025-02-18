41b623a38f72c4d2ce76def46b65b211ce7aeaa0
HoodKhan <muhammadhoodkhan9366@gmail.com>
2025-02-18 18:44:40 +0500

    array

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-let marks = Number(prompt('Please enter marks: '))
+// Grading system on the basis of marks
+// let marks = Number(prompt('Please enter marks: '))
 
-if(marks>=90 && marks<=100){
-    console.log("A1 Grade")
-}
-else if(marks>=80 && marks<90){
-    console.log('A Grade')
-}
-else if(marks>=70 && marks<80){
-    console.log('B Grade')
-}
-else if(marks>=60 && marks<70){
-    console.log('C Grade')
-}
-else if(marks>=50 && marks<60){
-    console.log('Pass')
-}
-else if(marks>=30 && marks<50){
-    console.log('Pass')
-}
-else if(marks>=0 && marks<30){
-    console.log("fail")
-}
-else{
-    console.log('invalid marks')
-}
+// if(marks>=90 && marks<=100){
+//     console.log("A1 Grade")
+// }
+// else if(marks>=80 && marks<90){
+//     console.log('A Grade')
+// }
+// else if(marks>=70 && marks<80){
+//     console.log('B Grade')
+// }
+// else if(marks>=60 && marks<70){
+//     console.log('C Grade')
+// }
+// else if(marks>=50 && marks<60){
+//     console.log('Pass')
+// }
+// else if(marks>=30 && marks<50){
+//     console.log('Pass')
+// }
+// else if(marks>=0 && marks<30){
+//     console.log("fail")
+// }
+// else{
+//     console.log('invalid marks')
+// }
+
+// login through if else
+// let userName = 'hood'
+// let pass = '123'
+
+let cities = ['peshawar', 'multan', 'karachi', 'quetta', 'lahore', 'islamabad', 'nowshehra', 'abbottabad', 'charsadda', 'mohmand']
+console.log(cities[4])
